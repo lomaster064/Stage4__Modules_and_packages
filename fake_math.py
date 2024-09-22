8aab374607293fe88de2193870295e30b3a5145a
lomaster064 <64ilya64@gmail.com>
2024-09-22 04:35:12 -0700

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,5 @@
+def divine(first, second):
+    if second == 0:
+        return 'Ошибка'
+    else:
+        return first / second
---
+++ module_4_1.py
@@ -0,0 +1,11 @@
+from fake_math import divine as div_f
+from true_math import divine as div_t
+
+result1 = div_f(69, 3)
+result2 = div_f(3, 0)
+result3 = div_t(49, 7)
+result4 = div_t(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
